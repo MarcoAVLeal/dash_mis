533fcad5beef606b334cee9b514ed3447264f405
Marco A. V. Leal <marcoaureliolv@gmail.com>
2022-02-24 14:41:17 -0300

    Add

--- server.R
+++ server.R
@@ -400,7 +400,7 @@ server <- function(input, output, session) {
         # scale_x_continuous(breaks = seq(0,1*input$cut_renda1,0.05*input$cut_renda1))
         # scale_y_continuous(breaks = seq(0,1,0.1))+
         axis.theme(title_size = 12,textsize = 12,pos_leg = "bottom",x.angle = 45,vjust = 1,hjust=1)
-      ggplotly(p1) %>% layout(hovermode = "x", spikedistance =  -1,margin = c(0,0,0,10),
+     plot <- ggplotly(p1) %>% layout(hovermode = "x", spikedistance =  -1,margin = c(0,0,0,10),
                               xaxis = list(title = "<b>Qntd. Envios</b>", showspikes = TRUE, titlefont = list(size = 24),
                                            spikemode  = 'across', #toaxis, across, marker
                                            spikesnap = 'cursor',  ticks = "outside",tickangle = -45,
@@ -412,7 +412,7 @@ server <- function(input, output, session) {
                                             showline=TRUE,tickfont = list(size = 24),fixedrange=TRUE,
                                             showgrid=TRUE),
                               autosize = T,height= 600) %>% config(displayModeBar = FALSE)
-      
+    plot  
       
     })
     
