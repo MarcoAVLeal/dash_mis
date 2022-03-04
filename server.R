9ce2c6fef2792120acb7662601f5d08610ac3d5c
Marco A. V. Leal <marcoaureliolv@gmail.com>
2022-03-04 12:26:42 -0300

    Fix 7

--- server.R
+++ server.R
@@ -437,7 +437,7 @@ server <- function(input, output, session) {
                    linetype = "dashed", colour = "red", alpha = 1,size = 0.8) +
         geom_hline(yintercept = sum(qtd_contatos_enviados),
                    linetype = "dashed", colour = "red", alpha = 1,size = 0.8) 
-     plot <- ggplotly(p1) %>% layout(hovermode = "x", spikedistance =  -1,margin = c(0,0,0,10),legend = list(x = 0.1, y = 0.9),
+     plot <- ggplotly(p1) %>% layout(hovermode = "x", spikedistance =  -1,margin = c(0,0,0,10),legend = list(x = 0.05, y = 1),
                               xaxis = list(title = "<b>Dias</b>", showspikes = TRUE, titlefont = list(size = 24),
                                            spikemode  = 'across', #toaxis, across, marker
                                            spikesnap = 'cursor',  ticks = "outside",tickangle = -45,
@@ -466,7 +466,7 @@ server <- function(input, output, session) {
       scale_x_continuous(breaks = seq(min(x1$dia),max(x1$dia),by = paste(length(unique(lubridate::month(x1$dia))),"days") ) )+
         # scale_y_continuous(breaks = seq(0,1,0.1))+
         axis.theme(title_size = 12,textsize = 12,pos_leg = "bottom",x.angle = 45,vjust = 1,hjust=1)
-      plot <- ggplotly(p1) %>% layout(hovermode = "y", spikedistance =  -1,margin = c(0,0,0,10),legend = list(x = 0.1, y = 0.9),
+      plot <- ggplotly(p1) %>% layout(hovermode = "y", spikedistance =  -1,margin = c(0,0,0,10),legend = list(x = 0.05, y = 1),
                                       xaxis = list(title = "<b>Dias</b>", showspikes = TRUE, titlefont = list(size = 24),
                                                    spikemode  = 'across', #toaxis, across, marker
                                                    spikesnap = 'cursor',  ticks = "outside",tickangle = -45,
@@ -503,7 +503,7 @@ server <- function(input, output, session) {
         scale_x_continuous(breaks = seq(min(x1$dia),max(x1$dia),by = paste(length(unique(lubridate::month(x1$dia))),"days") ) )+
         # scale_y_continuous(breaks = seq(0,1,0.1))+
         axis.theme(title_size = 12,textsize = 12,pos_leg = "bottom",x.angle = 45,vjust = 1,hjust=1)
-      plot <- ggplotly(p1) %>% layout(hovermode = "y", spikedistance =  -1,margin = c(0,0,0,10),legend = list(x = 0.1, y = 0.9),
+      plot <- ggplotly(p1) %>% layout(hovermode = "y", spikedistance =  -1,margin = c(0,0,0,10),legend = list(x = 0.05, y = 1),
                                       xaxis = list(title = "<b>Dias</b>", showspikes = TRUE, titlefont = list(size = 24),
                                                    spikemode  = 'across', #toaxis, across, marker
                                                    spikesnap = 'cursor',  ticks = "outside",tickangle = -45,
