68cd02787696c7b1d53da4416581084f4ffbd7b8
Marco A. V. Leal <marcoaureliolv@gmail.com>
2022-03-09 15:52:07 -0300

    fixpage5

--- pagina5.R
+++ pagina5.R
@@ -40,10 +40,11 @@ div(
                box(title = tags$p("Correlograma e Deferenciação",style = "font-size:100%;color:#E4781C;font-weight:bold;"),
                    headerBorder = TRUE,
                    width = 12,
-                   collapsible = TRUE,collapsed = TRUE,
+                   collapsible = TRUE,
+                   collapsed = TRUE,
                    solidHeader = TRUE,
                    plotOutput(outputId = "serie_diff_correlogram",height = 960)
-                   )
+                   ))
              
     )
     
