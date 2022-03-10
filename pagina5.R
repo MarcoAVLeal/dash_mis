c8aa9b368464120a1cdf0b246510563db701bc3b
Marco A. V. Leal <marcoaureliolv@gmail.com>
2022-03-10 15:19:14 -0300

    fixpage5

--- pagina5.R
+++ pagina5.R
@@ -49,13 +49,21 @@ div(
                  # type = "hidden",
                  
                  tabPanel(title = p("Correlograma e Diferencição",style="color:#E4781C;text-align:center;font-weight:bold;font-size:14px"), 
-                          box(title = tags$p("Correlograma e Diferenciação",style = "font-size:100%;color:#E4781C;font-weight:bold;"),
+                          box(title = tags$p("Série Produção",style = "font-size:100%;color:#E4781C;font-weight:bold;"),
                               headerBorder = TRUE,
                               width = 12,
                               collapsible = TRUE,
                               collapsed = TRUE,
                               solidHeader = TRUE,
                               plotOutput(outputId = "serie_diff_correlogram",height = 960)
+                          ),
+                          box(title = tags$p("Série Log Produção",style = "font-size:100%;color:#E4781C;font-weight:bold;"),
+                              headerBorder = TRUE,
+                              width = 12,
+                              collapsible = TRUE,
+                              collapsed = TRUE,
+                              solidHeader = TRUE,
+                              plotOutput(outputId = "serie_log_diff_correlogram",height = 960)
                           )
                  ),
                  tabPanel(title = p("Box-Plot",style="color:#E4781C;text-align:center;font-weight:bold;font-size:14px"), 
