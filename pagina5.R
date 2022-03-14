b32060ec581020163fbd47408a421380911a7512
Marco A. V. Leal <marcoaureliolv@gmail.com>
2022-03-14 15:13:26 -0300

    fixpage5

--- pagina5.R
+++ pagina5.R
@@ -13,7 +13,7 @@ div(
                                  collapsible = TRUE,
                                  solidHeader = TRUE,
                                  uiOutput("box_uiprod1")),
-                             box(title = tags$p("Prodção :",input$data_producao, " Até ",input$data_producao,style = "font-size:100%;color:#E4781C;font-weight:bold;"),
+                             box(title = tags$p("Produção :",input$data_producao, " Até ",input$data_producao,style = "font-size:100%;color:#E4781C;font-weight:bold;"),
                                  headerBorder = TRUE,
                                  width = 12,
                                  collapsible = TRUE,
